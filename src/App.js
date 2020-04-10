6a76151ff8802d5050297611cb64dde8d2f57a96
I353212 <jagmeet.singh03@sap.com>
2020-04-10 22:54:27 +0530

    Revert "Merge pull request #1 from sjagmeet37/mytestbranch"
    This reverts commit 8325159b545ac24bf9fe77ee77369df5f352ac3c, reversing
    changes made to 73824c6678929576925423b89b6f6fa855d07733.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import * as Icon from 'react-feather';
 
 import './App.scss';
 
-
-//test branch code 1
-//test branch code 1
-//test branch code 1
-//test branch code 1
-//test branch code 1
 import Home from './components/home';
 import Navbar from './components/navbar';
 import Links from './components/links';
